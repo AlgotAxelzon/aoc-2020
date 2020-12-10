738426fc5be73245b9e3c05ff180152dd6a57187
Algot Axelzon <algot.axelzon@gmail.com>
2020-12-10 09:06:01 +0100

    Tidy up

--- 10/solution.py
+++ 10/solution.py
@@ -5,18 +5,18 @@ def input():
 def ways(vals):
     ways_dict = {}
     for val in vals:
-        valids = [v for v in vals if 1 <= (val[0] - v[0]) <= 3]
-        valids_ways = sum([ways_dict[valid[1]] for valid in valids])
-        if val[0] == 1:
-            ways_dict[val[1]] = 1
-        elif 2 <= val[0] <= 3:
-            ways_dict[val[1]] = 1 + valids_ways
+        valids = [v for v in vals if 1 <= (val - v) <= 3]
+        valids_ways = sum([ways_dict[str(valid)] for valid in valids])
+        if val == 1:
+            ways_dict[str(val)] = 1
+        elif 2 <= val <= 3:
+            ways_dict[str(val)] = 1 + valids_ways
         else:
-            ways_dict[val[1]] = valids_ways
-    return ways_dict[max(vals)[1]]
+            ways_dict[str(val)] = valids_ways
+    return ways_dict[str(max(vals))]
 
 def sol2():
-    vals = [[int(x), x] for x in input()]
+    vals = [int(x) for x in input()]
     vals.sort()
     return ways(vals)
 
